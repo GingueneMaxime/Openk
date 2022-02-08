5d72cb435387a3452d642176cd231a858d4ec42a
user <user@DESKTOP-PNLGDQM>
2022-02-08 10:15:12 +0100

    Modif IHM

--- openk/src/main/java/ihm/bd/openkControl.java
+++ openk/src/main/java/ihm/bd/openkControl.java
@@ -2,7 +2,8 @@ package ihm.bd;
 
 
 
-
+import openk.Utilisateur;
+import ihm.bd.ControlGUI;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -20,12 +21,12 @@ public class openkControl {
 
 	// La partie droite de la fenêtre, une liste d'avions
 	@FXML
-	private TableView<Projet> projetTable;
+	private TableView<Utilisateur> utilisateurTable;
 	// pour laquelle on n'affiche que deux informations (identifiant et nom de l'avion)
 	@FXML
-	private TableColumn<Projet, String> idColonne;
+	private TableColumn<Utilisateur, String> idColonne;
 	@FXML
-	private TableColumn<Projet, String> nomColonne;
+	private TableColumn<Utilisateur, String> nomColonne;
 
 	// un champs qui permet de montrer l'action du bouton
 	@FXML
@@ -34,9 +35,7 @@ public class openkControl {
 	// Les champs de la partie gauche de la fenêtre qui affichent les détails 
 	// de l'avions actif
 	@FXML
-	private Label num_utilisateur;
-	@FXML
-	private Label nom;
+	private Label id;
 	@FXML
 	private Label prenom;
 	@FXML
@@ -45,7 +44,7 @@ public class openkControl {
 	private Label tel;
 
 	// Les références croisées vers l'application principale
-	private AvionGUI mainApp;
+	private ControlGUI mainApp;
 
 	// La ligne sélectionnée dans la liste (par défaut la première)
 	private int ligneActive=0;
@@ -66,7 +65,7 @@ public class openkControl {
 		// verrons pas en détail.
 		// Pour faire court : les méthodes sont appliquées pour chaque ligne avec chaque élément de la liste
 		// On passe par des SimpleStringProperty pour conserver nos classes métier sans javaFX
-		idColonne.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getNumero()+""));
+		idColonne.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getNum_utilisateur()+""));
 		nomColonne.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getNom()));
 	}
 
@@ -75,10 +74,10 @@ public class openkControl {
 	 * au sous-contrôleur
 	 * @param mainApp : l'application principale 
 	 */
-	public void setMainApp(AvionGUI mainApp) {
+	public void setMainApp(ControlGUI mainApp) {
 		//System.out.println("setMainApp called");
 		this.mainApp = mainApp;
-		projetTable.setItems(this.mainApp.getProjetData());
+		utilisateurTable.setItems(this.mainApp.getutilisateurData());
 		afficheAvionSelection();  
 	}
 
@@ -86,90 +85,17 @@ public class openkControl {
 	 * Mets à jour la partie gauche de la fenêtre avec les détails de l'avion actif 
 	 */
 	private void afficheAvionSelection() {
-		Projet projetCourant = projetTable.getItems().get(ligneActive);
-		num_utilisateur.setText(projetCourant.getNumero()+"");
+		Utilisateur projetCourant = utilisateurTable.getItems().get(ligneActive);
+		id.setText(projetCourant.getNum_utilisateur()+"");
 
-		nom.setText(projetCourant.getNom());
 		prenom.setText(projetCourant.getPrenom());
 		mail.setText(projetCourant.getMail());
 		tel.setText(projetCourant.getTel()+"");
 	}
-
-	/**
-	 * Pour chaque bouton on peut définir une méthode propre associée à ce bouton
-	 * Dans Scene Builder, à gauche, on clique sur hierarchy, Button
-	 * à droite, a été indiqué le nom de la méthode dans On Action
-	 * @param event
-	 */
-	@FXML
-	private void handleButtonAction(ActionEvent event) {
-		texteTest.setText("clic (" + ligneActive+")");
-	}
-
-	
-	/**
-	 * À vérifier
-	 * Il serait possible de générer des petites fenêtres de dialogue
-	 * mais actuellement la classe Alert est inconnue (nécessite javaFX 8u40)
-	 * @param event
-	 *
-	@FXML
-	private void handleButtonActionMessage(ActionEvent event) {
-		Alert alert = new Alert(AlertType.INFORMATION);
-		alert.setTitle("Information Dialog");
-		alert.setHeaderText("Look, an Information Dialog");
-		alert.setContentText("I have a great message for you!");
-
-		alert.showAndWait();
-	
-		alert = new Alert(AlertType.CONFIRMATION);
-		alert.setTitle("Confirmation Dialog");
-		alert.setHeaderText("Look, a Confirmation Dialog");
-		alert.setContentText("Are you ok with this?");
-
-		Optional<ButtonType> result = alert.showAndWait();
-		if (result.get() == ButtonType.OK){
-			texteTest.setText("option ok");
-		} else {
-			texteTest.setText("option cancel");
-		}	
-	}*/
-	@FXML
-	private void handleButtonActionMessage(ActionEvent event) {
-		texteTest.setText("Alert : inactif");
-	}
-	
-	/**
-	 * Pour le MenuButton, on peut définir une méthode propre à chaque ligne associée à cette liste
-	 * Dans Scene Builder, à gauche, on clique sur hierarchy, Button
-	 * à droite, a été indiqué le nom de la méthode dans On Action
-	 * @param event
-	 */
-	@FXML
-	public void handleButtonsListEpaves(ActionEvent event) {
-		projetTable.setItems(this.mainApp.getAvionDataEpave());
-		afficheAvionSelection(); 
-	}
-
-	@FXML
-	public void handleButtonsListRestore(ActionEvent event) {
-		projetTable.setItems(this.mainApp.getAvionDataAvion());
-		afficheAvionSelection(); 
-	}
-
-	/**
-	 * Pour chaque partie de la fenêtre qui peut être manipulée par la souris,
-	 * on peut faire une méthode.
-	 * Celle-ci est indiquée dans la partie "code" de TableView :
-	 * Dans Scene Builder, à gauche, on clique sur hierarchy, TableView
-	 * à droite, a été indiqué le nom de la méthode dans On Mouse Clicked
-	 *   
-	 * @param event
-	 */
 	@FXML
 	private void handleMouseTableViewAction(MouseEvent event) {
 		// permet de récupérer le numéro de la ligne sélectionnées
-		this.setLigneActive(projetTable.getSelectionModel().getSelectedIndex());
+		this.setLigneActive(utilisateurTable.getSelectionModel().getSelectedIndex());
 		this.afficheAvionSelection();
 	}
 
--- openk/src/main/java/openk/Projet.java
+++ openk/src/main/java/openk/Projet.java
@@ -1,7 +1,7 @@
 package openk;
 
 import dao.Connexion;
-
+import ihm.bd.ControlGUI;
 public class Projet {
 
 	public static void main(String[] args) {
