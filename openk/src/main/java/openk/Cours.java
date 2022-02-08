af7b8c8f05a3ec6384c0d774ca7c8d49070da39c
Utilisateur <Utilisateur@LAPTOP-C5ID9EIF>
2022-02-08 11:23:43 +0100

    Modif de Cours, Formation et Promotion

--- openk/src/main/java/openk/Cours.java
+++ openk/src/main/java/openk/Cours.java
@@ -1,5 +1,44 @@
 package openk;
 
-public class Cours {
+import java.time.LocalDateTime;
 
+public class Cours {
+	private int num_cours;
+	private int num_prof;
+	private int num_matiere;
+	private int num_promotion;
+	private LocalDateTime date_debut;
+	private LocalDateTime date_fin;
+
+	public int getNum_cours() {
+		return num_cours;
+	}
+	public void setNum_cours(int num_cours) {
+		this.num_cours = num_cours;
+	}
+	public int getNum_prof() {
+		return num_prof;
+	}
+	public int getNum_matiere() {
+		return num_matiere;
+	}
+	public int getNum_promotion() {
+		return num_promotion;
+	}
+	public LocalDateTime getDate_debut() {
+		return date_debut;
+	}
+	public LocalDateTime getDate_fin() {
+		return date_fin;
+	}
+	@Override
+	public String toString() {
+		return "Cours [num_cours=" + num_cours + ", num_prof=" + num_prof + ", num_matiere=" + num_matiere
+				+ ", num_promotion=" + num_promotion + ", date_debut=" + date_debut + ", date_fin=" + date_fin + "]";
+	}
+	
+	
+	
+	
+	
 }
--- openk/src/main/java/openk/Promotion.java
+++ openk/src/main/java/openk/Promotion.java
@@ -1,5 +1,33 @@
 package openk;
 
-public class Promotion {
+import java.time.LocalDateTime;
 
+public class Promotion {
+	
+	private int num_promotion;
+	private int num_formation;
+	private LocalDateTime date_debut;
+	private LocalDateTime date_fin;
+
+	public int getNum_promotion() {
+		return num_promotion;
+	}
+	public void setNum_promotion(int num_promotion) {
+		this.num_promotion = num_promotion;
+	}
+	public int getNum_formation() {
+		return num_formation;
+	}
+	public LocalDateTime getDate_debut() {
+		return date_debut;
+	}
+	public LocalDateTime getDate_fin() {
+		return date_fin;
+	}
+	@Override
+	public String toString() {
+		return "Promotion [num_promotion=" + num_promotion + ", num_formation=" + num_formation + ", date_debut="
+				+ date_debut + ", date_fin=" + date_fin + "]";
+	}
+	
 }
