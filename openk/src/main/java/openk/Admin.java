9dc268ab775ac26adc974e3f58ec8a16a22d9881
PC <PC@Maxime>
2022-02-08 08:48:32 +0100

    modif admin

--- openk/src/main/java/openk/Admin.java
+++ openk/src/main/java/openk/Admin.java
@@ -1,5 +1,21 @@
 package openk;
 
 public class Admin {
-
+	private int num_admin;
+	
+	public Admin(int id) {
+		
+		this.num_admin=num_admin;
+	}
+	public void setNumero(int numero) {
+		this.num_admin= numero;
+		
+	}
+	public int getNum_Admin() {
+		return num_admin;
+	}
+	@Override
+	public String toString() {
+		return "Admin[num_admin=" + num_admin+"]";
+	}
 }
