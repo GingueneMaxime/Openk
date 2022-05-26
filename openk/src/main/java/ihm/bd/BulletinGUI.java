567a440d1ff56d04bb92b38ef084189ac7dcdd08
PC <PC@Maxime>
2022-05-26 16:20:41 +0200

    modif bulletingui

--- openk/src/main/java/ihm/bd/BulletinGUI.java
+++ openk/src/main/java/ihm/bd/BulletinGUI.java
@@ -13,7 +13,6 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import openk.Eleve;
-import openk.Utilisateur;
 
 public class BulletinGUI extends Application {
 
